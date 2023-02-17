0e4a4d35ba7de57aa36e3aa28986d45a734c9c74
Izzet Ali <dralizzetyldrm@outlook.com>
2023-02-17 22:06:26 +0300

    created HandleFunc deleteMovie

--- main.go
+++ main.go
@@ -64,6 +64,18 @@ func updateMovie(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
 var movies []Movie
 
 func main() {
@@ -76,6 +88,7 @@ func main() {
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 	fmt.Printf("Starting server at port 8080\n")
 	log.Fatal(http.ListenAndServe(":8080", r))
 

88cd2281ce297626bf805ddee514c0e61029cead
Izzet Ali <dralizzetyldrm@outlook.com>
2023-02-17 21:54:37 +0300

    create server and mux router

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 func main() {
+	r := mux.NewRouter()
+
+	fmt.Printf("Starting server at port 8080\n")
+	log.Fatal(http.ListenAndServe(":8080", r))
 
 }

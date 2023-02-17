ebc981f18da6d7f3c45d365abbfc00ddaa8424bd
Izzet Ali <dralizzetyldrm@outlook.com>
2023-02-17 21:59:00 +0300

    created type Movie and Director

--- main.go
+++ main.go
@@ -8,8 +8,25 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+var movies []Movie
+
 func main() {
 	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "123456", Title: "Matrix", Director: &Director{FirstName: "John", LastName: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "123457", Title: "Matrix 2", Director: &Director{FirstName: "Steve", LastName: "Smith"}})
 
 	fmt.Printf("Starting server at port 8080\n")
 	log.Fatal(http.ListenAndServe(":8080", r))

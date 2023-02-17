2301a597a38cce6cd31215ad1d1fde377bab52e8
Izzet Ali <dralizzetyldrm@outlook.com>
2023-02-17 22:02:42 +0300

    created HandleFunc getMovie by id

--- main.go
+++ main.go
@@ -26,6 +26,17 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movies)
 }
 
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
 var movies []Movie
 
 func main() {
@@ -35,6 +46,7 @@ func main() {
 	movies = append(movies, Movie{ID: "2", Isbn: "123457", Title: "Matrix 2", Director: &Director{FirstName: "Steve", LastName: "Smith"}})
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	fmt.Printf("Starting server at port 8080\n")
 	log.Fatal(http.ListenAndServe(":8080", r))
 
